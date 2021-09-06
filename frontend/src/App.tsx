1fcb247d46e4accfaf0b64d984e8f7c16c244b67
Aldecir Lima <aldecirlima@gmail.com>
2021-09-06 09:31:15 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components";
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+
 function App() {
   return (
-    <div className="App">
-     <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
